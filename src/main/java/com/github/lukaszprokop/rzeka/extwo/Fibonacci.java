5568a01f5753fec7417d3cde7f1f61318cfcc751
lukasz <wuzu222@gmail.com>
2021-04-12 02:59:40 +0200

    Dodaj drugie zadanie

---
+++ src/main/java/com/github/lukaszprokop/rzeka/extwo/Fibonacci.java
@@ -0,0 +1,17 @@
+package com.github.lukaszprokop.rzeka.extwo;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        // fibo(5) powinno zwrócić 7 (4+3)
+        System.out.println(fibo(5));
+    }
+
+    private static int fibo(int n) {
+        //TODO: zaimplementuj tę metodę używając strumieni tak aby przyjmowała index `n`
+        // i zwracała liczbę z ciągu Fibonacciego która znajduje się pod tym intexem:
+        // dla n = 0 fibo zwraca 0
+        // dla n = 1 fibo zwraca 1
+        // dla n > 1 fibo zwraca (n-1)+(n-2)
+        return n;
+    }
+}
