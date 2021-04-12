a923871afae06e9bc1f932a3e4eba9fb388fdbe7
lukasz <wuzu222@gmail.com>
2021-04-12 02:20:28 +0200

    Dodaj pierwsze zadanie

---
+++ src/main/java/com/github/lukaszprokop/rzeka/exone/EqualityLambda.java
@@ -0,0 +1,19 @@
+package com.github.lukaszprokop.rzeka.exone;
+
+class EqualityLambda {
+
+    //TODO: Napraw program nie zmieniając metody 'main()'
+    public static void main(String[] args) {
+
+        Container<Integer, Integer, Integer> container =
+                (a, b, c) -> a.equals(b) && b.equals(c);
+
+        int a = 2 * 2;
+        int b = Math.addExact(2, 2);
+        int c = Math.multiplyExact(2, 2);
+
+        boolean check = container.check(a, b, c);
+
+        System.out.printf("check() powinien zwracać 'true' i zwraca: %s", check);
+    }
+}
